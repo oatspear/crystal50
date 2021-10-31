591498e5f60514e1ac110d3f7b13efdf871aeba0
git-afsantos <contact.andre.santos@gmail.com>
2021-10-31 21:52:59 +0000

    updated Sandstorm to 1/16 HP damage

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -1780,7 +1780,7 @@ HandleWeather:
 	ld de, ANIM_IN_SANDSTORM
 	call Call_PlayBattleAnim
 	call SwitchTurnCore
-	call GetEighthMaxHP
+	call GetSixteenthMaxHP
 	call SubtractHPFromUser
 
 	ld hl, SandstormHitsText
