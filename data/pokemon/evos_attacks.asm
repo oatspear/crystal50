0ee18b2bb318e83bd0bbe13f091367cd79dfe934
git-afsantos <contact.andre.santos@gmail.com>
2021-11-15 18:35:20 +0000

    moved commented code to its own file

--- data/pokemon/evos_attacks.asm
+++ data/pokemon/evos_attacks.asm
@@ -1,30 +1,5 @@
 INCLUDE "constants.asm"
 
-; useful script to get learnsets from Bulbapedia tables:
-; (function() {
-;     const out = [];
-;     let table = $("#By_leveling_up").parent().next();
-;     while (table.length != 0 && table[0].nodeName != "TABLE")
-;         table = table.next();
-;     let rows = table        // <table>
-;         .children().eq(0)   // <tbody>
-;         .children().eq(1)   // <tr>
-;         .children().eq(0)   // <td>
-;         .children().eq(0)   // <table>
-;         .children().eq(1)   // <tbody>
-;         .children();        // [<tr>]
-;     rows.each(function(i, r) {
-;         let cells = $(r).children();
-;         let lvl = cells.eq(0).contents().eq(1).text().trim();
-;         lvl = lvl.replace("Evo.", "LEVEL_EVO");
-;         let move = cells.eq(cells.length > 7 ? 2 : 1).children().children().text().trim();
-;         move = move.toUpperCase().replace(" ", "_").replace("-", "_");
-;         out.push(`\tdb ${lvl}, ${move}`);
-;     });
-;     console.log(out.join("\n"));
-; })();
-
-
 SECTION "Evolutions and Attacks", ROMX
 
 ; Evos+attacks data structure:
