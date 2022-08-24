f703017703a9a7ca9b2b99106d18e300caea9b73
oatspear <oatspear@gmail.com>
2022-08-24 08:40:27 +0100

    changed player names

--- data/player_names.asm
+++ data/player_names.asm
@@ -10,10 +10,10 @@ ChrisNameMenuHeader:
 	db 5 ; items
 	db "NEW NAME@"
 MalePlayerNameArray:
-	db "CHRIS@"
-	db "MAT@"
-	db "ALLAN@"
-	db "JON@"
+	db "OATS@"
+	db "BARLEY@"
+	db "WHEAT@"
+	db "GOLD@"
 	db 2 ; title indent
 	db " NAME @" ; title
 
@@ -29,9 +29,9 @@ KrisNameMenuHeader:
 	db 5 ; items
 	db "NEW NAME@"
 FemalePlayerNameArray:
-	db "KRIS@"
-	db "AMANDA@"
-	db "JUANA@"
-	db "JODI@"
+	db "OATS@"
+	db "ALMOND@"
+	db "SOY@"
+	db "CRYSTAL@"
 	db 2 ; title indent
 	db " NAME @" ; title
