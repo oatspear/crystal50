a027873d8b4d6aebf8ee1317455d03e187cdd53a
oatspear <oatspear@gmail.com>
2022-09-03 15:05:51 +0100

    swarm table

--- data/wild/swarm_grass.asm
+++ data/wild/swarm_grass.asm
@@ -34,28 +34,28 @@ SwarmGrassWildMons:
 	map_id ROUTE_35
 	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
 	; morn
-	db 12, NIDORAN_M
-	db 12, NIDORAN_F
+	db 14, PIDGEY
+	db 12, SNUBBULL
 	db 12, YANMA
 	db 14, YANMA
-	db 14, PIDGEY
-	db 10, DITTO
+	db 12, ELEKID
+	db 10, ABRA
 	db 10, DITTO
 	; day
-	db 12, NIDORAN_M
-	db 12, NIDORAN_F
+	db 14, PIDGEY
+	db 12, SNUBBULL
 	db 12, YANMA
 	db 14, YANMA
-	db 14, PIDGEY
-	db 10, DITTO
+	db 12, ELEKID
+	db 10, ABRA
 	db 10, DITTO
 	; nite
-	db 12, NIDORAN_M
-	db 12, NIDORAN_F
+	db 14, HOOTHOOT
+	db 12, DROWZEE
 	db 12, YANMA
 	db 14, YANMA
-	db 14, HOOTHOOT
-	db 10, DITTO
+	db 10, MAGNEMITE
+	db 12, MURKROW
 	db 10, DITTO
 
 	db -1 ; end
