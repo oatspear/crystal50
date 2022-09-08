a2e4aa28e362082f5c7738943bd03060150a2338
oatspear <oatspear@gmail.com>
2022-09-08 10:26:19 +0100

    rename const

--- constants/wram_constants.asm
+++ constants/wram_constants.asm
@@ -292,7 +292,7 @@ CELEBIEVENT_FOREST_IS_RESTLESS_F EQU 2
 
 ; wDailyFlags3::
 	const_def
-	const DAILYFLAGS3_VIRIDIAN_FOREST_WILD_MON_F          ; 0
+	const DAILYFLAGS3_VIRIDIAN_FOREST_FRUIT_TREE_MON_F    ; 0
 
 ; wSwarmFlags::
 	const_def
